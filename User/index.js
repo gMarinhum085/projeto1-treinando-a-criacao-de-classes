fc7dce15d1fb241a0740fca70aada31545b9cf36
Gabriel Marinho dos Santos <152182959+gMarinhum085@users.noreply.github.com>
2024-08-08 10:36:04 -0300

    Add files via upload

---
+++ User/index.js
@@ -0,0 +1,16 @@
+class User {           
+    constructor(fullname, email, password) {
+      this.fullname = fullname
+      this.email = email
+      this.password = password
+    }
+    login(email, password) {
+      if (this.email === email && this.password === password) {
+        console.log(`${this.fullname} Login sucedido!`)
+      } else {
+        console.log(`${this.fullname} O login falhou.`)
+      }
+    }
+  }
+
+  const user = new User('John', 'john@gmail.com', '1234567')
